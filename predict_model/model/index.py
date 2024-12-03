625ca8292c463c9a0c58f8026e0fcf93d94ccb26
bluewarn <n5lp97@gmail.com>
2024-12-03 10:37:41 +0000

    다중공산성 문제를 해결하기 위해 revenue와 높은 상관관계를 가지는 budget 변수 제거

--- predict_model/model/index.py
+++ predict_model/model/index.py
@@ -23,9 +23,9 @@ collection = db['movies']
 data = list(collection.aggregate([
     # { '$sample': { 'size': 3000 }}, # 랜덤으로 3000개의 문서를 가져온다.
     { '$project': { # RDB의 SELECT에 대응. 1이면 포함 0이면 제외, 학습에 필요한 속성만 가져온다.
-        "budget": 1,
         "revenue": 1,
         "runtime": 1,
+        "omdb_rate": 1,
         # OMDB를 통해 평점 평균 값도 넣어야 함.
         "_id": 0
     }}
@@ -43,13 +43,13 @@ print(train.head())
 top_10_revenue = train.sort_values(by='revenue', ascending=False).head(10)
 print(top_10_revenue)
 
-top_10_budget = train.sort_values(by='budget', ascending=False).head(10)
-print(top_10_budget)
+# top_10_budget = train.sort_values(by='budget', ascending=False).head(10)
+# print(top_10_budget)
 
-# budget 히스토그램과 KDE, 0부터 10퍼센트 구간만 자세히 보기
-sns.histplot(train.budget, kde=True, stat="density")
-plt.savefig("budget_히스토그램.png")
-plt.clf()
+# # budget 히스토그램과 KDE, 0부터 10퍼센트 구간만 자세히 보기
+# sns.histplot(train.budget, kde=True, stat="density")
+# plt.savefig("budget_히스토그램.png")
+# plt.clf()
 
 # revenue 히스토그램과 KDE, 0부터 10퍼센트 구간만 자세히 보기
 sns.histplot(train.revenue, kde=True, stat="density")
@@ -59,12 +59,12 @@ plt.clf()
 # 수치형 데이터만 선택, 상관 계수 계산.
 numeric_features = train.select_dtypes(include=[np.number])
 # 결측치 처리 (예: 평균값으로 대체)
-numeric_features = numeric_features.fillna(method='ffill')
+# numeric_features = numeric_features.fillna(method='ffill')
 correlation_matrix = numeric_features.corr()
 
-print(correlation_matrix)
+print("상관계수: ", correlation_matrix)
 
-sns.heatmap(correlation_matrix.corr(), cmap='YlGnBu', annot=True, linewidths = 0.2);
+sns.heatmap(numeric_features.corr(), cmap='YlGnBu', annot=True, linewidths = 0.2);
 plt.savefig("correlation_matrix.png")
 plt.clf()
 
@@ -73,26 +73,35 @@ sns.histplot(train.revenue, kde=True)
 plt.savefig("histplot_train.revenue.png")
 plt.clf()
 
-sns.jointplot(x = train.budget, y = train.revenue);
-plt.savefig("jointPlot_budget_revenue.png")
-plt.clf()
-sns.jointplot(x = train.runtime, y = train.revenue);
-plt.savefig("jointPlot_runtime_revenue.png")
+print('Descriptive Stats for the omdb_rate are:\n ', train.omdb_rate.describe())
+sns.histplot(train.omdb_rate, kde=True)
+plt.savefig("histplot_train.omdb_rate.png")
 plt.clf()
 
-sns.boxplot(x=train['budget'])
-plt.savefig("budget_박스플롯.png")
+# sns.jointplot(x = train.budget, y = train.omdb_rate);
+# plt.savefig("jointPlot_budget_omdb_rate.png")
+# plt.clf()
+sns.jointplot(x = train.runtime, y = train.omdb_rate);
+plt.savefig("jointPlot_runtime_omdb_rate.png")
 plt.clf()
+
+# sns.boxplot(x=train['budget'])
+# plt.savefig("budget_박스플롯.png")
+# plt.clf()
 
 sns.boxplot(x=train['revenue'])
 plt.savefig("revenue_박스플롯.png")
 plt.clf()
 
+sns.boxplot(x=train['omdb_rate'])
+plt.savefig("omdb_rate_박스플롯.png")
+plt.clf()
+
 # 모델 Building
 
 # 독립 변수와 종속 변수 분리
-X = train.drop(columns=['revenue'])  # 'revenue' 열을 종속 변수로 사용
-y = train['revenue']
+X = train.drop(columns=['omdb_rate'])  # 'revenue' 열을 종속 변수로 사용
+y = train['omdb_rate']
 
 # splitting the data into training and validation to check validity of the model
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_state=1)
@@ -100,6 +109,8 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1, random_
 # 데이터 확인
 print(f"학습 데이터 크기: {X_train.shape}")
 print(f"테스트 데이터 크기: {X_test.shape}")
+
+print(X_train)
 
 # Linear Model
 def rmsle(y,y0): return np.sqrt(np.mean(np.square(np.log1p(y)-np.log1p(y0)))) 
@@ -109,7 +120,7 @@ y_pred = reg.predict(X_test)
 print('RMSLE score for linear model is {}'.format(rmsle(y_test, y_pred)))
 pred1 = reg.predict(X_test)
 
-print(f'예측된 수익: {pred1[0]}')
+print(f'예측된 평점: {pred1[0]}')
 
 knn = KNeighborsRegressor(n_neighbors = 5)
 knn_model = knn.fit(X_train, y_train)
@@ -117,7 +128,7 @@ knn_y_pred = knn.predict(X_test)
 print('RMSLE score for k-NN model is {}'.format(rmsle(y_test, knn_y_pred)))
 pred2 = knn.predict(X_test)
 
-print(f'예측된 수익: {pred2[0]}')
+print(f'예측된 평점: {pred2[0]}')
 
 rf = RandomForestRegressor()
 rf_model = rf.fit(X_train, y_train)
@@ -125,4 +136,14 @@ rf_y_pred = rf.predict(X_test)
 print('RMSLE score for Random Forest model is {}'.format(rmsle(y_test, rf_y_pred)))
 pred3 = rf.predict(X_test)
 
-print(f'예측된 수익: {pred3[0]}')
+print(f'예측된 평점: {pred3[0]}')
+
+dic = pd.DataFrame({
+    # "budget": [74000000],
+    "runtime": [91],
+    "revenue": [1159457503],
+})
+
+print(lin_model.predict(dic))
+print(knn_model.predict(dic))
+print(rf_model.predict(dic))
